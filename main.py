030c6fb9689366826d0b9c3032fb327f61e4b5f3
nagino <mth55@nct9.ne.jp>
2024-05-13 23:20:12 +0900

    ああ

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import streamlit as st
 from PIL import Image
 
 
-st.title('IMDS')
-st.caption('チェックプログラム')
+st.title('設計管理　ツール')
+st.caption('左のサイドバーから　選択してください')
 
-image = Image.open('./data/ダウンロード.png')
-st.image(image, width=200)
+# image = Image.open('./data/ダウンロード.png')
+# st.image(image, width=200)
